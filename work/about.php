8b3fdf3fb88047e90a076ac7dbf27f66da043ac8
harishprakash <karam.harish249@gmail.com>
2017-12-06 17:57:24 +0530

    Master file

---
+++ work/about.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Vertex | About</title>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" media="screen" href="css/reset.css">
+<link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+<link href='http://fonts.googleapis.com/css?family=Muli:400,300,300italic,400italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Passion+One:400,700' rel='stylesheet' type='text/css'>
+<script src="js/jquery-1.7.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+<![endif]-->
+</head>
+<body>
+<div class="bg">
+  <div class="bg-2">
+    <!--==============================header=================================-->
+    <header>
+      <h1><a href="index.html"><img src="images/logo.png" alt=""></a></h1>
+      <nav>
+        <ul class="menu">
+          <li><a href="index.html">Home</a></li>
+          <li class="current"><a href="about.html">About</a></li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="products.html">Products</a></li>
+          <li><a href="contacts.php">Contacts</a></li>
+        </ul>
+      </nav>
+    </header>
+    <!--==============================content================================-->
+    <section id="content">
+      <div class="slogan top-1">
+        <p>We <span class="clr-1">provide</span> you with the <span class="clr-1">highest</span> level of <span class="clr-1">services</span></p>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit vivamus sed arcu dui eu tincidunt sem.</p>
+        </div>
+      <div class="wrap page2-row1">
+        <div class="page2-col-1 border-right">
+          <h2><span class="clr-1">W</span>hy us?</h2>
+          <ul class="list-1">
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Consectetur elit</a></li>
+            <li><a href="#">Vivamus sed a</a></li>
+            <li><a href="#">Ivamus hendrerit</a></li>
+            <li><a href="#">Gravida viverra</a></li>
+            <li><a href="#">Cras mattis</a></li>
+          </ul>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed arcu dui, eu tincidunt sem vivamus.</p>
+        </div>
+        <div class="page2-col-2 border-right">
+          <h2><span class="clr-1">W</span>ho we are?</h2>
+          <div class="wrap"> <img src="images/page2-img1.jpg" alt="" class="img-indent-2 img-border">
+            <p class=" extra-wrap"><strong class="clr-2">G N C S & Associates, Chartered Accountants, a leading audit and accounting firm, with offices in Chennai, Bangalore and Chittoor (Andhra Pradesh) is rendering comprehensive professional services in the areas of Direct Tax and Indirect Tax Consultancy, Payroll processing, Accounting services, Statutory, Internal and Management Audit, tax reporting and other Regulatory Compliances.</p>
+          </div>
+          <p>The firms Head Office is in Chittoor and branches are at Chennai and Bangalore fully equipped with modern infrastructure and are backed by a team of highly qualified, competent and experienced personnel. </p>
+          
+          <p>Excellence, Integrity and Independence, the Motto of our Institute of Chartered Accountants of India, is the ultimate objective of the Firm in all its professional commitments.</p>
+
+          <p>The firm provides not only audit, tax, and accounting, but also a broad range of consulting services to meet our clients' growing needs. These consulting services represent our commitment to go beyond the audit and provide "value added" services that will increase the efficiency and effectiveness of your operations.</p>
+
+          <p>In today's complex financial environment, the firm you select should provide more than auditing services. It should be your advisor - a partner you can turn to year-round, whenever you are faced with a decision that will affect your financial future.</p>
+          </div>
+        <div class="page2-col-3">
+          <h2><span class="clr-1">Q</span>uality Policy</h2>
+          <div class="box-3">
+            <div class="comment">
+              <ul class="list-1">
+                <li>To build a solid base with flexible policies to create an environment for innovation and development.</li>
+                <li>To deepen the meaning of quality to each member of the firm regardless of his/her job description.</li>
+                <li>To progress the standard level of commitment to clients in fulfilling their demands.</li>
+              </ul>
+          </div>
+          
+      </div>
+    </section>
+  </div>
+</div>
+<!--==============================aside=================================-->
+<aside>
+  <div class="wrap">
+    <div class="aside-col-1 border-right-2">
+      <h3><span class="clr-1">N</span>ewsletter:</h3>
+      <form id="form-search" method="post" action="#">
+        <span>Enter you email here:</span>
+        <input type="text" value="" onBlur="if(this.value=='') this.value=''" onFocus="if(this.value =='' ) this.value=''">
+        <a href="#" class="link-2">subscribe</a>
+      </form>
+    </div>
+    <div class="aside-col-2 border-right-2">
+      <h3><span class="clr-1">U</span>seful info:</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur. adipiscing elit. Vivamus sed arcu dui, eu tincidunt sem viamus hendrerit. </p>
+    </div>
+    <div class="aside-col-3 border-right-2">
+      <dl class="adrss">
+        <dd><span>Freephone:</span><strong>+1 800 559 6580</strong></dd>
+        <dd><span>Telephone:</span><strong>+1 800 603 6035</strong></dd>
+        <dd><span>Fax:</span><strong>+1 800 889 9898</strong></dd>
+        <dd><span>E-mail:</span><a href="#" class="link"><strong>mail@demolink.org</strong></a></dd>
+      </dl>
+      <p><strong>9870St Vincent Place,<br>
+        Glasgow, DC 45 Fr 45.</strong></p>
+    </div>
+    <div class="aside-col-4">
+      <div class="soc-icons"> <a href="#"><img src="images/icon-1.png" alt=""></a><a href="#"><img src="images/icon-2.png" alt=""></a><a href="#"><img src="images/icon-3.png" alt=""></a> </div>
+    </div>
+  </div>
+</aside>
+<!--==============================footer=================================-->
+<footer>
+  <p><strong>© 2012 Vertex</strong><br>
+    Website Template by <a target="_blank" href="http://www.templatemonster.com/" class="link-4">TemplateMonster.com</a></p>
+</footer>
+</body>
+</html>
---
+++ work/contacts.php
@@ -0,0 +1,121 @@
+<?php 
+include("include/header.php");
+?>
+<!-- <link rel="stylesheet" href="js/leaflet.css" />
+  <script src="js/leaflet.js"></script> -->
+
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css" integrity="sha512-M2wvCLH6DSRazYeZRIm1JnYyh22purTM+FDB5CsyxtQJYeKq83arPe5wgbNmcFXGqiSH2XR8dT/fJISVA1r/zQ==" crossorigin=""/>
+  <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js" integrity="sha512-lInM/apFSqyy1o6s89K4iQUKg6ppXEgsVxT35HbzUupEVRh2Eu9Wdl4tHj7dZO0s1uvplcYGmt3498TtHq+log==" crossorigin=""></script>
+
+  <section id="content">
+   <div class="slider">
+     <div id="mapid" style="width: 100%; height: 400px;"></div>
+     <script>
+
+      var mymap = L.map('mapid').setView([13.062723, 80.168113], 13);
+
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.streets'
+      }).addTo(mymap);
+
+      L.marker([13.062723, 80.168113]).addTo(mymap);
+
+      L.marker([13.062723, 80.168113], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+      }).addTo(mymap);
+
+
+
+
+    </script>
+
+  </div>
+  <div class="slogan top-1">
+    <p>We <span class="clr-1">provide</span> you with the <span class="clr-1">highest</span> level of <span class="clr-1">services</span></p>
+  </div>
+
+  <div class="page5-row1">
+    <div class="page5-col-1 border-right">
+      <h2><span class="clr-1">C</span>ontact info</h2>
+      <dl class="adr">
+        <dt class="clr-1"><strong>GNCS ASSOCIATES</strong></dt>
+        <dd><strong>Plot No.53, Door No. 2, G Floor,1st Street, Pallavan Nagar,Maduravoyal, Chennai - 600 095.
+        </strong></dd>
+        <dd><span>Telephone:</span><strong>044 - 4857 4423/4500 4423/64550224</strong></dd>
+        <dd><span>Mobile:</span><strong>+91 98840 28900 / 90804 33540 / 98667 71444.</strong></dd>
+        <dd><span>Email:</span><a href="mailto:gncsassociates@gmail.com" class="link" target="_blank">gncsassociates@gmail.com</a></dd>
+        <dd>&nbsp;</dd>
+       <!--  <dd><strong>9863 Mill Road, Cambridge, MG09 99HT</strong></dd>
+        <dd><span>Telephone:</span><strong>+1 959 603 6035</strong></dd>
+        <dd><span>Fax:</span><strong>+1 504 889 9898</strong></dd>
+        <dd><span>Email:</span><a href="#" class="link">mail@thomsander.com</a></dd> -->
+      </dl>
+    </div>
+    <div class="page5-col-2">
+      <h2><span class="clr-1">C</span>ontact form:</h2>
+      <form id="form" method="post" action="#">
+        <fieldset>
+          <label><strong>Full name:</strong>
+            <input type="text" value="">
+            <strong class="clear"></strong></label>
+            <label><strong>Email:</strong>
+              <input type="text" value="">
+              <strong class="clear"></strong></label>
+              <label><strong>Message:</strong>
+                <textarea></textarea>
+                <div class="btns">
+                <button type="reset" class="link-2" value="clear">Clear</button>
+                <button type="submit" class="link-2" value="Send">Send</button>
+                </div>
+               <!-- <strong class="clear"></strong></label>
+                <strong class="clear"></strong>
+                <div class="btns"><a href="#" class="link-2">Clear</a><a href="#" class="link-2">Send</a></div> -->
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+  <!--==============================aside=================================-->
+<!-- <aside>
+  <div class="wrap">
+    <div class="aside-col-1 border-right-2">
+      <h3><span class="clr-1">N</span>ewsletter:</h3>
+      <form id="form-search" method="post" action="#">
+        <span>Enter you email here:</span>
+        <input type="text" value="" onBlur="if(this.value=='') this.value=''" onFocus="if(this.value =='' ) this.value=''">
+        <a href="#" class="link-2">subscribe</a>
+      </form>
+    </div>
+    <div class="aside-col-2 border-right-2">
+      <h3><span class="clr-1">U</span>seful info:</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur. adipiscing elit. Vivamus sed arcu dui, eu tincidunt sem viamus hendrerit. </p>
+    </div>
+    <div class="aside-col-3 border-right-2">
+      <dl class="adrss">
+        <dd><span>Freephone:</span><strong>+1 800 559 6580</strong></dd>
+        <dd><span>Telephone:</span><strong>+1 800 603 6035</strong></dd>
+        <dd><span>Fax:</span><strong>+1 800 889 9898</strong></dd>
+        <dd><span>E-mail:</span><a href="#" class="link"><strong>mail@demolink.org</strong></a></dd>
+      </dl>
+      <p><strong>9870St Vincent Place,<br>
+        Glasgow, DC 45 Fr 45.</strong></p>
+    </div>
+    <div class="aside-col-4">
+      <div class="soc-icons"> <a href="#"><img src="images/icon-1.png" alt=""></a><a href="#"><img src="images/icon-2.png" alt=""></a><a href="#"><img src="images/icon-3.png" alt=""></a> </div>
+    </div>
+  </div>
+</aside> -->
+
+
+<?php 
+include("include/footer.php");
+?>

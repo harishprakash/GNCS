6a638911bab5e9cd204a67f721230401f7a91efd
harishprakash <karam.harish249@gmail.com>
2017-12-11 18:43:17 +0530

    Add files via upload

--- work/about.php
+++ work/about.php
@@ -1,81 +1,90 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Vertex | About</title>
-<meta charset="utf-8">
-<link rel="stylesheet" type="text/css" media="screen" href="css/reset.css">
-<link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
-<link href='http://fonts.googleapis.com/css?family=Muli:400,300,300italic,400italic' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Passion+One:400,700' rel='stylesheet' type='text/css'>
-<script src="js/jquery-1.7.min.js"></script>
-<script src="js/jquery.easing.1.3.js"></script>
-<!--[if lt IE 9]>
-<script src="js/html5.js"></script>
-<link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
-<![endif]-->
-</head>
-<body>
-<div class="bg">
-  <div class="bg-2">
-    <!--==============================header=================================-->
-    <header>
-      <h1><a href="index.html"><img src="images/logo.png" alt=""></a></h1>
-      <nav>
-        <ul class="menu">
-          <li><a href="index.html">Home</a></li>
-          <li class="current"><a href="about.html">About</a></li>
-          <li><a href="services.html">Services</a></li>
-          <li><a href="products.html">Products</a></li>
-          <li><a href="contacts.php">Contacts</a></li>
-        </ul>
-      </nav>
-    </header>
-    <!--==============================content================================-->
-    <section id="content">
-      <div class="slogan top-1">
-        <p>We <span class="clr-1">provide</span> you with the <span class="clr-1">highest</span> level of <span class="clr-1">services</span></p>
-        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit vivamus sed arcu dui eu tincidunt sem.</p>
-        </div>
-      <div class="wrap page2-row1">
-        <div class="page2-col-1 border-right">
-          <h2><span class="clr-1">W</span>hy us?</h2>
-          <ul class="list-1">
-            <li><a href="#">Lorem ipsum</a></li>
-            <li><a href="#">Consectetur elit</a></li>
-            <li><a href="#">Vivamus sed a</a></li>
-            <li><a href="#">Ivamus hendrerit</a></li>
-            <li><a href="#">Gravida viverra</a></li>
-            <li><a href="#">Cras mattis</a></li>
-          </ul>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed arcu dui, eu tincidunt sem vivamus.</p>
-        </div>
-        <div class="page2-col-2 border-right">
-          <h2><span class="clr-1">W</span>ho we are?</h2>
-          <div class="wrap"> <img src="images/page2-img1.jpg" alt="" class="img-indent-2 img-border">
-            <p class=" extra-wrap"><strong class="clr-2">G N C S & Associates, Chartered Accountants, a leading audit and accounting firm, with offices in Chennai, Bangalore and Chittoor (Andhra Pradesh) is rendering comprehensive professional services in the areas of Direct Tax and Indirect Tax Consultancy, Payroll processing, Accounting services, Statutory, Internal and Management Audit, tax reporting and other Regulatory Compliances.</p>
-          </div>
-          <p>The firms Head Office is in Chittoor and branches are at Chennai and Bangalore fully equipped with modern infrastructure and are backed by a team of highly qualified, competent and experienced personnel. </p>
-          
-          <p>Excellence, Integrity and Independence, the Motto of our Institute of Chartered Accountants of India, is the ultimate objective of the Firm in all its professional commitments.</p>
-
-          <p>The firm provides not only audit, tax, and accounting, but also a broad range of consulting services to meet our clients' growing needs. These consulting services represent our commitment to go beyond the audit and provide "value added" services that will increase the efficiency and effectiveness of your operations.</p>
-
-          <p>In today's complex financial environment, the firm you select should provide more than auditing services. It should be your advisor - a partner you can turn to year-round, whenever you are faced with a decision that will affect your financial future.</p>
-          </div>
-        <div class="page2-col-3">
-          <h2><span class="clr-1">Q</span>uality Policy</h2>
-          <div class="box-3">
-            <div class="comment">
-              <ul class="list-1">
-                <li>To build a solid base with flexible policies to create an environment for innovation and development.</li>
-                <li>To deepen the meaning of quality to each member of the firm regardless of his/her job description.</li>
-                <li>To progress the standard level of commitment to clients in fulfilling their demands.</li>
-              </ul>
-          </div>
-          
-      </div>
-    </section>
+<?php 
+include("include/header.php");
+?>
+<section id="content">
+  <div class="slogan top-1">
+    <p>We <span class="clr-1">provide</span> you with the <span class="clr-1">highest</span> level of <span class="clr-1">services</span></p>
   </div>
+  <div class="wrap page2-row1">
+    <div class="page2-col-1 border-right">
+      <h2><span class="clr-1">O</span>ur Vision</h2>
+      <ul class="list-1">
+        <li><a href="#">Government Audits</a></li>
+        <li><a href="#">Medical Devices-Pharmaceutical Companies-Health Care</a></li>
+        <li><a href="#">Construction</a></li>
+        <li><a href="#">Distribution Agencies</a></li>
+        <li><a href="#">Aqua related industries</a></li>
+        <li><a href="#">Leather related industries</a></li>
+        <li><a href="#">Granite Industry</a></li>
+        <li><a href="#">Poultry Industry</a></li>
+        <li><a href="#">Concurrent and Stock Audits of various Banks</a></li>
+        <li><a href="#">ROC Search, Creation  & Satisfaction of Charge to Various Banks</a></li>
+        <li><a href="#">Due Diligence Audit</a></li>
+        <li><a href="#">Hotel Industry</a></li>
+        <li><a href="#">Advertising Agency</a></li>
+        <li><a href="#">Service Industry</a></li>
+        <li><a href="#">Micro Finance</a></li>
+        <li><a href="#">Multimedia</a></li>
+        <li><a href="#">Home Appliances</a></li>
+        <li><a href="#">Expat Taxation</a></li>
+        <li><a href="#">International Taxation</a></li>
+
+
+      </ul>
+      <p>We are proud to be a professionally managed, service oriented and knowledge based firm. We strive for our excellence keeping in mind Quality is Priority.</p>
+      <p>We are doing stock audits for Kotak Mahindra Bank Limited, Oriental Bank of Commerce, Axis Bank Limited and Standard Chartered Bank. Not only this we are doing ROC related works viz., Charge creation, ROC Search and Satisfaction of charge etc., to various banks.</p>
+    </div>
+    <div class="page2-col-2 border-right">
+      <h2><span class="clr-1">W</span>ho we are?</h2>
+      <div class="wrap"> <img src="images/page2-img1.jpg" alt="" class="img-indent-2 img-border">
+        <p class=" extra-wrap"><strong class="clr-2">G N C S & Associates, Chartered Accountants, a leading audit and accounting firm, with offices in Chennai, Bangalore and Chittoor (Andhra Pradesh) is rendering comprehensive professional services in the areas of Direct Tax and Indirect Tax Consultancy, Payroll processing, Accounting services, Statutory, Internal and Management Audit, tax reporting and other Regulatory Compliances.</p>
+      </div>
+      <p>The firms Head Office is in Chittoor and branches are at Chennai and Bangalore fully equipped with modern infrastructure and are backed by a team of highly qualified, competent and experienced personnel. </p>
+
+      <p>Excellence, Integrity and Independence, the Motto of our Institute of Chartered Accountants of India, is the ultimate objective of the Firm in all its professional commitments.</p>
+
+      <p>The firm provides not only audit, tax, and accounting, but also a broad range of consulting services to meet our clients' growing needs. These consulting services represent our commitment to go beyond the audit and provide "value added" services that will increase the efficiency and effectiveness of your operations.</p>
+
+      <p>In today's complex financial environment, the firm you select should provide more than auditing services. It should be your advisor - a partner you can turn to year-round, whenever you are faced with a decision that will affect your financial future.</p>
+
+      <h2><span class="clr-1">P</span>artners Profile:</h2>
+      <h3 style="color: green;">CA S. Chenchu Rathna Reddy, M.Com, F.C.A.</h3>
+      <p>Mr. S.C.R.Reddy after obtaining his Bachelors Degree in Commerce from S.V. University, Tirupati and did his article ship in M/s. S P Associates, Chennai. He obtained his Masters Degree in Commerce in the year 2005. He qualified as CA in the year 2006 and he is the Managing Partner of the firm. CA SCR Reddy has eight years of post qualification experience in the field of accounting, auditing, financial services, company law matters, Due Diligence Audit and indirect tax laws.</p>
+      <p>
+        His ability to understand the business environment coupled with his sound financial and accounting background has enabled him to specialize in conducting statutory audits and internal audits of the companies. He has an in depth knowledge of the Pharmaceuticals, Banks, Hotel, Construction, Home Appliances and Automobile industry.
+      </p><p>
+      Apart from advising client on various issues under the Income-tax Act, Wealth Tax act and Indirect Tax Laws, he has a vast experience in handling assessment proceedings under the Income Tax Act, Central Excise, Service tax Act and Sales Tax/VAT Act. 
+    </p><p>
+    Membership No: 219556
+  </p><p>
+  E-mail: cascrreddy@gmail.com
+</p>
+
+<h3 style="color: green;">CA Geetha N, B.Com, F.C.A.</h3>
+<p>Ms. Geetha. N is a Commerce Graduate (Taxation) from S.V.University and qualified as CA in the year 2006. CA Geetha who is one of the founders of the firm has enrolled as member of ICAI in 2008 and she is the Managing Partner for Chennai Branch. She has seven years of post qualification experience in Tax Audit, Statutory Audit, Internal Audit, Stock audit, Bank audit, ROC related works and legal compliances.
+</p><p>
+CA Geetha. N has good knowledge of automobile industry, construction industry, food processing industry, home appliances distribution and pharmaceuticals industry. She has vast experience in the legal compliance, management audit and internal audit. 
+</p><p>
+Membership No: 220963
+</p><p>
+E-mail: cageetha@gmail.com
+</p>
+</div>
+
+<div class="page2-col-3">
+  <h2><span class="clr-1">Q</span>uality Policy</h2>
+  <div class="box-3">
+    <div class="comment">
+      <ul class="list-1">
+        <li>To build a solid base with flexible policies to create an environment for innovation and development.</li>
+        <li>To deepen the meaning of quality to each member of the firm regardless of his/her job description.</li>
+        <li>To progress the standard level of commitment to clients in fulfilling their demands.</li>
+      </ul>
+    </div>
+
+  </div>
+</section>
+</div>
 </div>
 <!--==============================aside=================================-->
 <aside>
@@ -101,16 +110,16 @@
       </dl>
       <p><strong>9870St Vincent Place,<br>
         Glasgow, DC 45 Fr 45.</strong></p>
+      </div>
+      <div class="aside-col-4">
+        <div class="soc-icons"> <a href="#"><img src="images/icon-1.png" alt=""></a><a href="#"><img src="images/icon-2.png" alt=""></a><a href="#"><img src="images/icon-3.png" alt=""></a> </div>
+      </div>
     </div>
-    <div class="aside-col-4">
-      <div class="soc-icons"> <a href="#"><img src="images/icon-1.png" alt=""></a><a href="#"><img src="images/icon-2.png" alt=""></a><a href="#"><img src="images/icon-3.png" alt=""></a> </div>
-    </div>
-  </div>
-</aside>
-<!--==============================footer=================================-->
-<footer>
-  <p><strong>© 2012 Vertex</strong><br>
-    Website Template by <a target="_blank" href="http://www.templatemonster.com/" class="link-4">TemplateMonster.com</a></p>
-</footer>
-</body>
-</html>
+  </aside>
+  <!--==============================footer=================================-->
+  <footer>
+    <p><strong>© 2012 Vertex</strong><br>
+      Website Template by <a target="_blank" href="http://www.templatemonster.com/" class="link-4">TemplateMonster.com</a></p>
+    </footer>
+  </body>
+  </html>
